a8e5f9b0dc884034cc9ccc637f600d10b196052c
Enrique <taguniversalsoftware@gmail.com>
2024-08-24 21:59:17 -0500

    make separate dockerfiles for test and build

--- test/digitalblockchain_web/controllers/page_controller_test.exs
+++ test/digitalblockchain_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DigitalblockchainWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Digital Blockchain"
   end
 end
