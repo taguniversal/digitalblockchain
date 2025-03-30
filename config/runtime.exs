1a058e9f77a4d5e883377cdb04b2f4718f453665
Enrique <taguniversalsoftware@gmail.com>
2025-03-29 21:55:48 -0500

    set new domain

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "digitalblockchain.io"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   # Tigris
@@ -60,7 +60,9 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: ["https://digitalblockchain.io"],
+    check_origin: ["https://www.digitalblockchain.io"]
 
   # ## SSL Support
   #
